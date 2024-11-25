9038a63e83ce717bd776e919fe2317f5ee157695
Devit Roy <ashraicselab@gmail.com>
2024-11-25 12:24:02 +0600

    1st version

---
+++ Calculator_awt.java
@@ -0,0 +1,14 @@
+
+package calculator_awt;
+
+public class Calculator_awt {
+
+    
+     
+    public static void main(String[] args) {
+        System.out.println("Hi Java");
+        
+    }
+    
+}
+
