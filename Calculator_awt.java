3d4fb38bbeb38cb45ba9b5aae0b9f310f76f79e8
Devit Chandra Roy <ashraicselab@gmail.com>
2024-11-25 14:44:10 +0600

    Update Calculator_awt.java

--- Calculator_awt.java
+++ Calculator_awt.java
@@ -8,6 +8,7 @@ public class Calculator_awt {
     public static void main(String[] args) {
         System.out.println("Hi Java");
         System.out.println("Hi Java from versaion 2");
+        System.out.println("Java Program");
         
     }
     
